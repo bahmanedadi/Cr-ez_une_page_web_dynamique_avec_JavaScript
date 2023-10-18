49623c26ff54995eb7be0e360cec2ab7856d2835
Dadi B <benmezianedadi@yahoo.fr>
2023-10-18 22:01:32 +0200

    Suppression de la gallery du HTML

--- FrontEnd/assets/scripts/index.js
+++ FrontEnd/assets/scripts/index.js
@@ -1,6 +1,16 @@
-const url = 'http://localhost:5678/api/works';
+/*const url = 'http://localhost:5678/api/works';
 
 fetch(url) // aller chercher la donnée 
 .then(response => {
     return response.json(); // extraire toutes les données
-})
+})*/
+/* Récupérez l'élément parent avec la classe "gallery"*/
+
+var galleryDiv = document.querySelector('.gallery');
+document.addEventListener('DOMContentLoaded', function () {
+    /* Vérifiez si l'élément parent existe*/
+    if (galleryDiv) {
+        /* Supprimez tous les enfants de l'élément parent*/
+        galleryDiv.innerHTML = '';
+    }
+});
