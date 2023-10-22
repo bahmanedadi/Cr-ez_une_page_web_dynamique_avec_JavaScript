de3395352d979eca2d973fa80d0c609a2bc550c5
Dadi B <benmezianedadi@yahoo.fr>
2023-10-23 00:06:39 +0200

    ajout des filtres et css

--- FrontEnd/assets/scripts/index.js
+++ FrontEnd/assets/scripts/index.js
@@ -1,9 +1,9 @@
 const url = 'http://localhost:5678/api/works';
-
-fetch(url) // aller chercher la donnée 
-.then(response => {
-    return response.json(); // extraire toutes les données
-})
+/*aller chercher la donnée */
+fetch(url)
+    .then(response => {
+        return response.json(); // extraire toutes les données
+    })
 /* Récupérez l'élément parent avec la classe "gallery"*/
 
 var galleryDiv = document.querySelector('.gallery');
@@ -14,3 +14,19 @@ document.addEventListener('DOMContentLoaded', function () {
         galleryDiv.innerHTML = '';
     }
 });
+
+const divBtns = document.querySelector('.btns');
+
+/* Tableau contenant le texte pour chaque bouton */
+const labels = ['tous', 'objets', 'Appartements', 'Hotels & Restaurants'];
+
+for (let i = 0; i < labels.length; i++) {
+    const bouton = document.createElement('button');
+    bouton.innerText = labels[i];
+    bouton.classList.add('btn_' + (i + 1));
+    /* Action à effectuer lors du clic sur le bouton*/
+    bouton.addEventListener('click', function () {
+        alert(labels[i] + ' cliqué !'); 
+    });
+    divBtns.appendChild(bouton);
+}
