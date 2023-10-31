96fddf4c67b47cbae40be129f27edb85029c09fc
Dadi B <benmezianedadi@yahoo.fr>
2023-10-31 17:40:23 +0100

    travail sur la visibility du login et logout

--- FrontEnd/assets/scripts/index.js
+++ FrontEnd/assets/scripts/index.js
@@ -3,89 +3,139 @@ let categoriesData = [];
 
 /* Récupération des données des travaux*/
 fetch('http://localhost:5678/api/works')
-  .then(response => response.json())
-  .then(data => {
-    worksData = data;
-    /* Appel d'une fonction pour ajouter les travaux à la galerie*/
-    addWorksToGallery(worksData);
-    console.log(worksData);
-  });
+    .then(response => response.json())
+    .then(data => {
+        worksData = data;
+        /* Appel d'une fonction pour ajouter les travaux à la galerie*/
+        addWorksToGallery(worksData);
+        console.log(worksData);
+    });
 
 /* Récupération des données des catégories*/
 fetch('http://localhost:5678/api/categories')
-  .then(response => response.json())
-  .then(data => {
-    categoriesData = data;
-    /* Appel d'une fonction pour créer les boutons de catégorie*/
-    createCategoryButtons(categoriesData);
-  });
+    .then(response => response.json())
+    .then(data => {
+        categoriesData = data;
+        /* Appel d'une fonction pour créer les boutons de catégorie*/
+        createCategoryButtons(categoriesData);
+    });
 
 /* Fonction pour ajouter les travaux à la galerie*/
 function addWorksToGallery(works) {
-  const galerie = document.querySelector('.gallery');
-  galerie.innerHTML = ''; 
+    const galerie = document.querySelector('.gallery');
+    galerie.innerHTML = '';
 
-  works.forEach(work => {
-    const figure = document.createElement('figure');
-    const image = document.createElement('img');
-    image.src = work.imageUrl;
-    image.alt = work.title;
-    const legende = document.createElement('figcaption');
-    legende.textContent = work.title;
-    figure.appendChild(image);
-    figure.appendChild(legende);
-    galerie.appendChild(figure);
-  });
+    works.forEach(work => {
+        const figure = document.createElement('figure');
+        const image = document.createElement('img');
+        image.src = work.imageUrl;
+        image.alt = work.title;
+        const legende = document.createElement('figcaption');
+        legende.textContent = work.title;
+        figure.appendChild(image);
+        figure.appendChild(legende);
+        galerie.appendChild(figure);
+    });
 }
 
 /* Fonction pour créer les boutons de catégorie*/
 function createCategoryButtons(categories) {
     const divBtns = document.querySelector('.btns');
-    const galerie = document.querySelector('.gallery'); 
-  
-    categories.forEach(categorie => {
-      const bouton = document.createElement('button');
-      bouton.innerText = categorie.name;
-      bouton.classList.add('btn');
-      bouton.addEventListener('click', function () {
-        const categorieSelectionnee = categorie.name;
-        
-        /*Filtrer les travaux en fonction de la catégorie sélectionnée*/
-        const travauxFiltres = worksData.filter(work => work.category.name === categorieSelectionnee);
-        
-        /* Affichez les travaux filtrés dans la galerie*/
-        addWorksToGallery(travauxFiltres);
-        console.log(travauxFiltres);
-       /* Retirez la classe "active" de tous les boutons de catégorie*/
-      const allButtons = document.querySelectorAll('.btn');
-      allButtons.forEach(button => {
-        button.classList.remove('active');
-      });
-
-      /* Ajoutez la classe "active" uniquement au bouton cliqué*/
-      bouton.classList.add('active');
-    });
-    divBtns.appendChild(bouton);
-  });
-}
-  
- 
-  
-  /* Fonction pour ajouter les travaux à la galerie*/
-  function addWorksToGallery(works) {
     const galerie = document.querySelector('.gallery');
-    galerie.innerHTML = ''; 
-  
-    works.forEach(work => {
-      const figure = document.createElement('figure');
-      const image = document.createElement('img');
-      image.src = work.imageUrl;
-      image.alt = work.title;
-      const legende = document.createElement('figcaption');
-      legende.textContent = work.title;
-      figure.appendChild(image);
-      figure.appendChild(legende);
-      galerie.appendChild(figure);
+
+    categories.forEach(categorie => {
+        const bouton = document.createElement('button');
+        bouton.innerText = categorie.name;
+        bouton.classList.add('btn');
+        bouton.addEventListener('click', function () {
+            const categorieSelectionnee = categorie.name;
+
+            /*Filtrer les travaux en fonction de la catégorie sélectionnée*/
+            const travauxFiltres = worksData.filter(work => work.category.name === categorieSelectionnee);
+
+            /* Affichez les travaux filtrés dans la galerie*/
+            addWorksToGallery(travauxFiltres);
+            console.log(travauxFiltres);
+            /* Retirez la classe "active" de tous les boutons de catégorie*/
+            const allButtons = document.querySelectorAll('.btn');
+            allButtons.forEach(button => {
+                button.classList.remove('active');
+            });
+
+            /* Ajoutez la classe "active" uniquement au bouton cliqué*/
+            bouton.classList.add('active');
+        });
+        divBtns.appendChild(bouton);
     });
-  }
-  
+}
+
+
+
+/* Fonction pour ajouter les travaux à la galerie*/
+function addWorksToGallery(works) {
+    const galerie = document.querySelector('.gallery');
+    galerie.innerHTML = '';
+
+    works.forEach(work => {
+        const figure = document.createElement('figure');
+        const image = document.createElement('img');
+        image.src = work.imageUrl;
+        image.alt = work.title;
+        const legende = document.createElement('figcaption');
+        legende.textContent = work.title;
+        figure.appendChild(image);
+        figure.appendChild(legende);
+        galerie.appendChild(figure);
+    });
+}
+
+const boutonTous = document.querySelector('.btn_1');
+boutonTous.addEventListener('click', function () {
+    /*Affichez tous les travaux dans la galerie (sans filtre par catégorie)*/
+    addWorksToGallery(worksData);
+    console.log(worksData);
+
+    /* Retirez la classe "active" de tous les boutons de catégorie */
+    const allButtons = document.querySelectorAll('.btn');
+    allButtons.forEach(button => {
+        button.classList.remove('active');
+    });
+
+    /* Ajoutez la classe "active" uniquement au bouton cliqué */
+    boutonTous.classList.add('active');
+});
+
+
+if (localStorage.getItem("token")) {
+    document.getElementById("login").innerHTML = '<a id="logout">logout</a>';
+    /// Pour rendre la div btns visible
+    document.querySelector('.btns').style.display = 'none';
+    // Sélectionnez l'élément de la modale et le bouton pour la fermer
+const modal = document.getElementById("myModal");
+const closeButton = document.querySelector(".close");
+
+
+
+// Affichez la modale
+modal.style.display = "block";
+
+// Fermez la modale lorsque l'utilisateur clique sur le bouton de fermeture
+closeButton.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+// Fermez la modale lorsque l'utilisateur clique en dehors de la modale
+window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
+
+    document.getElementById("logout").addEventListener("click", () => {
+        localStorage.removeItem("token");
+        document.getElementById("login").innerHTML = '<a href="login.html">login</a>'
+    });
+} else {
+    document.getElementById("login").innerHTML = '<a href="login.html">login</a>'
+    document.querySelector('.btns').style.display = 'block';
+}
--- FrontEnd/assets/scripts/login.js
+++ FrontEnd/assets/scripts/login.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    /* Sélection des éléments du formulaire et des messages d'erreur*/
     const loginForm = document.querySelector('.login_form');
     const emailInput = document.getElementById('email');
     const passwordInput = document.getElementById('password');
@@ -6,70 +7,82 @@ document.addEventListener('DOMContentLoaded', function () {
     const loginMdpError = document.querySelector('.loginMdp_error');
     const loginErreur = document.querySelector('.error');
 
+    /* Écouteur d'événement pour la soumission du formulaire*/
     loginForm.addEventListener('submit', async function (e) {
         e.preventDefault();
 
-        loginEmailError.textContent = '';
-        loginMdpError.textContent = '';
-        loginErreur.textContent = '';
+        /* Réinitialisation des messages d'erreur*/
+        loginEmailError.textContent = "";
+        loginMdpError.textContent ="";
+        loginErreur.textContent = "";
 
+        /* Récupération des valeurs du formulaire*/
         const email = emailInput.value;
         const password = passwordInput.value;
 
+        /* Validation de l'email et du mot de passe*/
         if (!validateEmail(email)) {
-            showError(loginEmailError, 'Veuillez entrer une adresse email valide');
+            showError(loginEmailError, "Veuillez entrer une adresse email valide");
             return;
         }
 
         if (!validatePassword(password)) {
-            showError(loginMdpError, 'Veuillez entrer un mot de passe valide');
+            showError(loginMdpError, "Veuillez entrer un mot de passe valide");
             return;
         }
 
         try {
+            /* Envoi des données de connexion au serveur via fetch*/
             const response = await fetch('http://localhost:5678/api/users/login', {
-                method: 'POST',
+                method: "POST",
                 headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
                 },
                 body: JSON.stringify({ email, password })
             });
 
             const responseData = await response.json();
 
+            /* Traitement des réponses du serveur*/
             if (response.status === 200) {
-                localStorage.setItem('token', responseData.token);
-                window.location.href = 'index.html';
+                /* Connexion réussie : stockage du token et redirection vers la page d'accueil*/
+                localStorage.setItem("token", responseData.token);
+                window.location.href = "index.html";
             } else if (response.status === 401) {
-                showError('Not Authorized');
+                /* Erreur d'autorisation*/
+                showError(loginErreur, "Not Authorized");
             } else if (response.status === 404) {
-                showError('User not found');
+                /* Utilisateur non trouvé*/
+                showError(loginErreur, "User not found");
             } else {
-                throw new Error('Erreur de requête');
+                /* Autres erreurs*/
+                throw new Error("Erreur de requête");
             }
         } catch (error) {
-            console.error('Une erreur s\'est produite :', error);
+            /* Gestion des erreurs de requête*/
+            console.error(" Une erreur s\'est produite :", error);
         }
     });
 
-
-
-    if (localStorage.getItem('token')) {
-        showError(loginEmailError, 'Vous avez été déconnecté, veuillez vous reconnecter');
+    /* Vérification de la présence d'un token dans le stockage local*/
+    if (localStorage.getItem("token")) {
+        showError(loginErreur, "Vous avez été déconnecté, veuillez vous reconnecter");
     }
 
+    /* Fonction de validation de l'email*/
     function validateEmail(email) {
         return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$/g.test(email);
     }
 
+    /* Fonction de validation du mot de passe*/
     function validatePassword(password) {
         return /^[a-zA-Z0-9]+$/g.test(password) && password.length >= 6;
     }
-
+    /* Fonction pour afficher les messages d'erreur*/
     function showError(element, message) {
         const p = document.createElement('p');
-        p.innerHTML = message;
+        p.textContent = message;
         element.appendChild(p);
     }
 });
